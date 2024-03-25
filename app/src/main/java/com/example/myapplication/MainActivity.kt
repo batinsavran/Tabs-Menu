cf4a56c26b298b1023174837511bd51dd8017861
Batın Gökçe Savran <batinsavran00@gmail.com>
2024-03-25 08:55:39 +0300

    Passcodes for the pages that will open when the home page and tab items are selected have been written.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -12,11 +12,48 @@ import com.example.myapplication.databinding.FragmentPastJobBinding // PastJobFr
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityHomeBinding
+
+    private fun goPastJobFragment() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainerView, PastJobFragment())
+            .commit()
+    }
+
+    private fun goActiveJobFragment() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainerView, ActiveJobFragment())
+            .commit()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        goPastJobFragment()
+
+        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                tab?.let {
+                    when (it.position) {
+                        0 -> {
+                            goPastJobFragment()
+                        }
+                        1 -> {
+                            goActiveJobFragment()
+                        }
+                    }
+                }
+            }
+
+            override fun onTabUnselected(p0: TabLayout.Tab?) {
+                // Boş bırakıldı
+            }
+
+            override fun onTabReselected(p0: TabLayout.Tab?) {
+                // Boş bırakıldı
+            }
+        })
     }
 }
